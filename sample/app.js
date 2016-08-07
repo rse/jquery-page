8b691292a38bde5c8f5207d71dfafb156653497b
Ralf S. Engelschall <rse@engelschall.com>
2016-08-07 12:54:19 +0200

    fix sample

--- sample/app.js
+++ sample/app.js
@@ -5,7 +5,7 @@
         $(".screen .page .navigate").click(function (ev) {
             var page  = $(ev.target).attr("data-page-name");
             var trans = $(ev.target).attr("data-page-trans");
-            if ($(".screen").page().page(page) === null)
+            if ($(".screen").page().fetch(page) === null)
                 $(".screen").page().shake();
             else
                 $(".screen").page().transition(page, trans);
