80aa10a5bad518090a0be7194bb8e01b3b598e6f
Ralf S. Engelschall <rse@engelschall.com>
2021-01-09 16:58:41 +0100

    bump year in all copyright messages

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /*
 **  jQuery Page -- jQuery Page Transitions for HTML5 Single-Page-Apps
-**  Copyright (c) 2016-2019 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**  Copyright (c) 2016-2021 Dr. Ralf S. Engelschall <rse@engelschall.com>
 **
 **  Permission is hereby granted, free of charge, to any person obtaining
 **  a copy of this software and associated documentation files (the
